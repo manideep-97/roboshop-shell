cab3a10356bca365e74c3b03cdbbcb43c843f28f
manideep <manideepdhamera@gmail.com>
2023-02-16 15:06:57 +0000

    frontend in RoboShop

--- frontend.sh
+++ frontend.sh
@@ -6,4 +6,4 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 systemctl enable nginx
-systemctl start nginx
+systemctl start
